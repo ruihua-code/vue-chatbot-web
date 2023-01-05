df0e179800c35e3f617a75bf26252ad74d13af72
zhangruihua <zrhcode@163.com>
2023-01-05 20:24:20 +0800

    添加一个登录页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
-
+import LoginView from '../views/login/index.vue';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: LoginView,
+    },
     {
       path: '/',
       name: 'home',
---
+++ src/views/login/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="page-container">
+    <div class="login">
+      <div class="header">登录</div>
+      <el-form v-model="formData" label-width="60px">
+        <el-form-item label="用户名" prop="user">
+          <el-input v-model="formData.user" type="text" autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="formData.password" type="password" autocomplete="off" />
+        </el-form-item>
+        <el-form-item>
+          <div class="btn-login">
+            <el-button type="primary" @click="onLogin" :loading="isLoading">登录</el-button>
+          </div>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { reactive, ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const isLoading = ref(false);
+const formData = reactive({
+  user: '',
+  password: '',
+});
+
+const onLogin = () => {
+  console.log(formData.password, formData.user);
+  isLoading.value = true;
+  setTimeout(() => {
+    router.replace('/home');
+  }, 1500);
+};
+</script>
+
+<style lang="scss" scoped>
+@import './index';
+</style>
